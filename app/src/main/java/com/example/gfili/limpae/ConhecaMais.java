963c1fc5bada08a4ddfe6ec8fb459d259f812b59
gabrielfisilva <838fx478>
2018-03-02 00:20:02 -0300

    Conheca mais + Passe Conosco

---
+++ app/src/main/java/com/example/gfili/limpae/ConhecaMais.java
@@ -0,0 +1,13 @@
+package com.example.gfili.limpae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConhecaMais extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conheca_mais);
+    }
+}
---
+++ app/src/main/java/com/example/gfili/limpae/PasseConosco.java
@@ -0,0 +1,13 @@
+package com.example.gfili.limpae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasseConosco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passe_conosco);
+    }
+}
